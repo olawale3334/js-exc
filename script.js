428cd3a5408c1f9535b8705c3a8edd0fad735d69
olawale3334 <olawale3334@gmail.com>
2025-03-29 22:50:34 +0100

    new added

--- script.js
+++ script.js
@@ -24,4 +24,17 @@ num(2);*/
 //  }
 //  console.log(power (5,3)); //5x5x5=125
 
- prompt('what is your name', "name:")
+ //console.log(  prompt('what is your name', "name:"))
+
+ 
+ //console.log(confirm("continue learning js/"))
+//  console.log("hey i am being logged to console");
+//  console.warn("this is a warning");
+//  console.error("this is an error");
+
+//  //alertabc("this is an error")
+
+//  console.log(" %c hey i am being logged to console", "text-red; font-50px");
+  alert("js is cool");
+console.log(  confirm("continu learning js"));
+console.log(  prompt("what country are from?", "country name:"))
