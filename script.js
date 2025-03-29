a3f269ce27aada74ded52037fd6df482ce777732
olawale3334 <olawale3334@gmail.com>
2025-03-29 22:14:44 +0100

    new added

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 
 //document.getElementById("update").innerHTML = "i have been through alot";
- alert('error');
+ //alert('error');
 // it is recomended to add ; before new statment
-['hey there', "welcome"].forEach(alert);
+//['hey there', "welcome"].forEach(alert);
+
+
+// checkif the value is an even number
+/*function num (value){
+
+ if (value%2 == 0) {
+    console.log(true)
+ } else {
+    console.log(false)
+ }
+
+
+}
+
+num(2);*/
+//  function  power(num1,num2){
+//       return Math.pow(num1, num2);
+
+//  }
+//  console.log(power (5,3)); //5x5x5=125
+
+ prompt('what is your name', "name:")
