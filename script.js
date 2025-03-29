570ea7313e3e078cf566add14f971e4a2b0cabb2
olawale3334 <olawale3334@gmail.com>
2025-03-29 11:05:36 +0100

    new added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("hello");
+
+//document.getElementById("update").innerHTML = "i have been through alot";
+ alert('error');
+// it is recomended to add ; before new statment
+['hey there', "welcome"].forEach(alert);
